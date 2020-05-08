70d72870e5102109cd200c56059b0e3acc820220
cjocelene <cjocelene@gmail.com>
2020-05-08 17:27:29 +0000

    loc_state beam transform

---
+++ loc_state_beam.py
@@ -0,0 +1,120 @@
+import logging
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+
+class FormatStateFn(beam.DoFn):
+    def process(self, element):
+        states_abv = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'}
+
+        state_record = element
+        # print(state_record)
+        state_fips_code = state_record.get('state_fips_code')
+        state = state_record.get('state')
+        
+        if state is not None:
+            state = states_abv.get(state)
+            return [{'state_fips_code':state_fips_code, 'state':state}]
+        return [{'state_fips_code':state_fips_code, 'state':state}]
+          
+def run():
+    PROJECT_ID = 'spry-cosine-266801' 
+    options = {
+     'project': PROJECT_ID
+    }
+    opts = beam.pipeline.PipelineOptions(flags=[], **options)
+
+    # Create beam pipeline using local runner
+    p = beam.Pipeline('DirectRunner', options=opts)
+
+    sql = 'SELECT state_fips_code, state FROM covid19_usafacts_modeled.loc_state limit 20'
+    bq_source = beam.io.BigQuerySource(query=sql, use_standard_sql=True)
+
+    query_results = p | 'Read from BigQuery' >> beam.io.Read(bq_source)
+    query_results | 'Write log 1' >> WriteToText('input.txt')
+    
+    # apply ParDo to format the date of movie  
+    formatted_state_pcoll = query_results | 'Format State' >> beam.ParDo(FormatStateFn())
+
+    # write PCollection to log file
+    formatted_state_pcoll | 'Write log 2 for state' >> WriteToText('formatted_state_pcoll.txt')
+     
+    
+    dataset_id = 'covid19_usafacts_modeled'
+    table_id = 'loc_state_Beam'
+    schema_id = 'state_fips_code:INTEGER, state:STRING'
+
+    # write PCollection to new BQ table
+    
+    formatted_state_pcoll | 'Write BQ table' >> beam.io.WriteToBigQuery(dataset=dataset_id, 
+                                                  table=table_id, 
+                                                  schema=schema_id,
+                                                  project=PROJECT_ID,
+                                                  create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                                                  write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+                                                  batch_size=int(100))
+     
+    result = p.run()
+    result.wait_until_finish()      
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
